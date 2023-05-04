87966f40408ce9caed369b69b57841a6ec2e23cc
yuzuryuu <chelleang27@gmail.com>
2023-05-04 14:57:01 -0400

    Added Day One Folder
    Excercise 1-3

---
+++ Day One/Excercise1.py
@@ -0,0 +1,4 @@
+# Exercise 1 - Printing
+
+print("Day 1 - Python Print Function\nThe function is declared like this:")
+print("print('what to print')")
---
+++ Day One/Excercise3.py
@@ -0,0 +1,4 @@
+# Exercise 3 - Input Function
+# Write a program that prints the number of characters in a user's name.
+
+print(len(input("What is your name? ")))

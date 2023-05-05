a1c5c20139974df6ec7e2770ef2f7e45025123c3
yuzuryuu <chelleang27@gmail.com>
2023-05-05 18:30:50 -0400

    tip calculator
    project for day 2

---
+++ Day Two/day2project.py
@@ -0,0 +1,9 @@
+totalBill = input("How much was the bill?\n$")
+people = input("\nHow many people to split the bill?\n")
+tip = input("\nHow much tip would you like to give?\n")
+tipPercent = int(tip) / 100
+
+splitBill = float(totalBill) / int(people)
+eachPersonBill = (float(splitBill) * (float(tipPercent)+1))
+
+print(f"\nEach person should pay: ${eachPersonBill:.2f}")
